38608d8e479eb688b5356541c1ec3a06171f92f3
harinineon <harinineon22@gmail.com>
2024-07-16 18:14:29 +0530

    implement all the features

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,14 @@
-import { Inter } from "next/font/google";
+import React from "react";
 import type { Metadata } from "next";
-import "../globals.css";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
+import "../globals.css";
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import Bottombar from "@/components/shared/Bottombar";
 import RightSidebar from "@/components/shared/RightSidebar";
 import Topbar from "@/components/shared/Topbar";
-import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,19 +23,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       <html lang='en'>
         <body className={inter.className}>
-        <Topbar />
-            <main className='flex flex-row'>
-                <LeftSidebar />
-                    <section className='main-container'>
-                    <div className='w-full max-w-4xl'>{children}</div>
-                    </section>
-        {/* @ts-ignore */}
-                <RightSidebar />
-            </main>
-        <Bottombar />
+          <Topbar />
+
+          <main className='flex flex-row'>
+            <LeftSidebar />
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
+            </section>
+            {/* @ts-ignore */}
+            <RightSidebar />
+          </main>
+
+          <Bottombar />
         </body>
       </html>
     </ClerkProvider>
--- lib/models/user.model.ts
+++ lib/models/user.model.ts
@@ -1,43 +1,39 @@
 import mongoose from "mongoose";
 
-const userSchema= new mongoose.Schema({
-    id:{
-        type:String,
-        required:true,
+const userSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: String,
+  bio: String,
+  threads: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Thread",
     },
-    username:{
-        type:String,
-        unique:true,
-        required:true,
+  ],
+  onboarded: {
+    type: Boolean,
+    default: false,
+  },
+  communities: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Community",
     },
-    name:{
-        type:String,
-        required:true,
-    },
-    image:String,
-    bio:String,
-    threads:[
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"Thread",
-        },
-    ],
-    onboarded:{
-        type:Boolean,
-        default:false,
-    },
-    communities:[
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref:"Community",
-        },
-
-    ],
+  ],
 });
 
-const User=mongoose.models.User||mongoose.model("User",userSchema);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 
 export default User;
-
-// mongoose Schema Types ObjectId means single user has multiple threads and communities
-//ref reference to instance in db
